42cd5aec503f47f27baeabf4341b4098d2362ceb
Shiwei Chen <april.chen.0615@gmail.com>
2023-01-02 11:54:20 -0800

    Added metatags and basic semantic HTML structure.

--- reserve-a-table/src/App.js
+++ reserve-a-table/src/App.js
@@ -2,9 +2,57 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <nav>
+          {
+            /*
+              Everything that should be in navbar component
+              <menu>
+                <li><img><a href=""></a></img></li>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservation</li>
+                <li>Order Online</li>
+                <li>Login</li>
+              </menu>
+            */
+          }
+      </nav>
+
+      <header>
+        <article>
+            {/*Put the "order online" component here*/}
+        </article>
+      </header>
+
+      <main>
+          <section>
+            {
+              // "Specials" Component
+            }
+          </section>
+
+          <section>
+            {
+              // " Testimonials" Component
+            }
+          </section>
+
+          <section>
+            {
+              // "About us" Component
+            }
+          </section>
+      </main>
+
+      <footer>
+          {
+            // "image" "Navigation" "Contact" and "Connect" components
+            // done perhaps through flexbox, or ul + li lists
+          }
+      </footer>
+    </>
   );
 }
 
