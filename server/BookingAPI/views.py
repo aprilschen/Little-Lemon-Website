0e77bf9e7dcb47e14f1baf9fedc540920755f7aa
Shiwei Chen <april.chen.0615@gmail.com>
2023-01-30 02:41:06 -0800

    Implemented Email Confirmations

--- server/BookingAPI/views.py
+++ server/BookingAPI/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import BookingSerializer
+from django.conf import settings
+from django.core.mail import send_mail
 # Create your views here.
 
 @api_view(['GET', 'POST'])
@@ -9,7 +11,35 @@ def BookTime(request):
     if request.method=='GET':
         return Response({"message": "Success!"})
     if request.method=='POST':
-        serialized_item = BookingSerializer(data = request.data)
-        serialized_item.is_valid(raise_exception=True)
-        serialized_item.save()
-        return Response({"message": "POST Success!"})
+        booking = BookingSerializer(data = request.data)
+        booking.is_valid(raise_exception=True)
+        booking.save()
+
+        subject = 'Little Lemon Reservation'
+        message = """Hi {},
+Thank you for booking a reservation at Little Lemon. Unfortunately, this restaurant (and by proxy, your booking here) isn't real.
+The website and its functionalities was made by Shiwei (https://github.com/aprilschen). Go and follow him/her!
+Your reservation information is as follows:
+Name: {} {}
+Phone number: {}
+Number of People: {}
+Date: {}
+Time: {}
+Additional Comments: {}
+
+best,
+Little Lemon Robot <3
+""".format(booking.data['first_name'],
+           booking.data['first_name'],
+           booking.data['last_name'],
+           booking.data['phone_number'],
+           booking.data['people'],
+           booking.data['date'],
+           booking.data['time'],
+           booking.data['additional_comments'],
+    )
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [booking.data['email'],]
+        send_mail(subject, message, email_from, recipient_list)
+
+        return Response({"message": 'Email: {}'.format(booking.data['email'])})
--- server/server/settings.py
+++ server/server/settings.py
@@ -134,3 +134,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'littlelemonapiservice@gmail.com'
+EMAIL_HOST_PASSWORD = 'qlodqcfkyevtolli'
